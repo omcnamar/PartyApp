5701e4d597f223bedff264072fbb38198643a60e
Oleg McNamara <omcnamar@students.kennesaw.edu>
2017-08-30 19:34:24 -0400

    deleting comments

--- app/src/main/java/com/olegsagenadatrytwo/partyapp/MainActivity.java
+++ app/src/main/java/com/olegsagenadatrytwo/partyapp/MainActivity.java
@@ -6,20 +6,10 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Log.d(TAG, "onCreate: Pinche Segundo" );
-        Log.d(TAG, "onCreate: Rodrigo" );
-        Log.d(TAG, "onCreate: " + "test1");
-        Log.d(TAG, "onCreate: Test2" );
-        //edited from web
-
-        // TODO: 8/30/17 testing
-        //alksjdfl;kasjdf
 
     }
 }
