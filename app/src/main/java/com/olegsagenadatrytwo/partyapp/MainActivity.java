3a040bd61d81ee53e08dae8d2abb83f5ffbf23cb
RockDrigo10 <rodrigo.chavez10@gmail.com>
2017-08-30 18:54:53 -0400

    Commit Test2

--- app/src/main/java/com/olegsagenadatrytwo/partyapp/MainActivity.java
+++ app/src/main/java/com/olegsagenadatrytwo/partyapp/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "onCreate: Pinche Segundo" );
         Log.d(TAG, "onCreate: Rodrigo" );
+        Log.d(TAG, "onCreate: Test2" );
         //edited from web
     }
 }
