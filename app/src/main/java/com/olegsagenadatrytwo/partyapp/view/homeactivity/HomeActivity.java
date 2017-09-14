23ca9765c91971e5a4f800c3cdfda94309c5aa23
olayg <ogureje@yahoo.com>
2017-09-14 00:30:29 -0400

    Added toolbar which still needs design changes, also some other few tweaks

--- app/src/main/java/com/olegsagenadatrytwo/partyapp/view/homeactivity/HomeActivity.java
+++ app/src/main/java/com/olegsagenadatrytwo/partyapp/view/homeactivity/HomeActivity.java
@@ -1,15 +1,47 @@
 package com.olegsagenadatrytwo.partyapp.view.homeactivity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.olegsagenadatrytwo.partyapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class HomeActivity extends AppCompatActivity implements HomeActivityContract.view {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_actions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_profile:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
