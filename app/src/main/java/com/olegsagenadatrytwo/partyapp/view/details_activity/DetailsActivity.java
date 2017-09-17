13a7f22628f39901e49a657085bbb054fad8d0fc
olayg <ogureje@yahoo.com>
2017-09-17 17:20:59 -0400

    added details activity but missing google-services.json

---
+++ app/src/main/java/com/olegsagenadatrytwo/partyapp/view/details_activity/DetailsActivity.java
@@ -0,0 +1,15 @@
+package com.olegsagenadatrytwo.partyapp.view.details_activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.olegsagenadatrytwo.partyapp.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
