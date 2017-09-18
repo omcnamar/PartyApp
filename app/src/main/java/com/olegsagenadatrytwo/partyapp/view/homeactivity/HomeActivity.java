3b28293799fbbbf754502d3c6693bb734a71d24f
Gallus AWA <Gallus awagdp@gmail.com>
2017-09-18 00:19:10 -0400

    finished toolbar for now... map and location actions need work

--- app/src/main/java/com/olegsagenadatrytwo/partyapp/view/homeactivity/HomeActivity.java
+++ app/src/main/java/com/olegsagenadatrytwo/partyapp/view/homeactivity/HomeActivity.java
@@ -1,20 +1,25 @@
 package com.olegsagenadatrytwo.partyapp.view.homeactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.olegsagenadatrytwo.partyapp.R;
 import com.olegsagenadatrytwo.partyapp.inject.home_activity.DaggerHomeActivityComponent;
 import com.olegsagenadatrytwo.partyapp.model.eventbrite.Event;
 import com.olegsagenadatrytwo.partyapp.model.eventbrite.EventbriteEvents;
+import com.olegsagenadatrytwo.partyapp.view.loginactivity.LoginActivity;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HomeActivity extends AppCompatActivity implements HomeActivityContract.view {
 
@@ -67,5 +72,27 @@ public class HomeActivity extends AppCompatActivity implements HomeActivityContr
             }
         });
 
+    }
+
+    @OnClick({R.id.action_map, R.id.action_location, R.id.action_profile})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.action_map:
+                // TODO: 9/17/17 implement the Map View
+                Toast.makeText(this, "Map", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_location:
+                Toast.makeText(this, "Location", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.action_profile:
+                Intent loginIntent = new Intent(this, LoginActivity.class);
+                startActivity(loginIntent);
+                // TODO: 9/17/17 need to implement back button for profile class
+                break;
+
+        }
+    }
+
+    public void goToLocation(View view) {
     }
 }
