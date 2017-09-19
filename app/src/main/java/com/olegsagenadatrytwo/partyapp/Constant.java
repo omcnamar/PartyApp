18c14b9baa03909a523341f3b970a7f0302b736f
aaronhoskins2011 <aaronhoskins2011@gmail.com>
2017-09-19 00:30:51 -0400

    Initial integration of map and location utilities, constants, and a empty map fragment

--- app/src/main/java/com/olegsagenadatrytwo/partyapp/Constant.java
+++ app/src/main/java/com/olegsagenadatrytwo/partyapp/Constant.java
@@ -19,4 +19,30 @@ public class Constant {
     public static final String GEOLOCATION_BASE_URL = "";
     public static final String GOOGLEMAPS_BASE_URL = "";
 
+    //API Keys
+    public static final String GOOGLE_GEO_API_KEY = "AIzaSyDCrxkQzvF8OKqFR5HFeLlqPZiD-8l0jUY";
+    //Misc inforamtion
+    public static final double RADIUS_OF_EARTH= 637;//radius of earth in Km
+    public static final double METERS_TO_MILES_RATIO = 0.000621371;
+    public static final double MILES_TO_METERS_RATIO = 1609.344;
+    //Log Tags
+    public static final String TAG_LOCATION_INFO = "TAG_Location_info";
+    public static final String TAG_MARKER_COLOR_SET = "TAG_Marker_Color_Set";
+    public static final String TAG_MARKER_LOCATION_SET = "TAG_Marker_Loc_Set";
+    public static final String TAG_IMAGE_MARKER_SET = "TAG_Marker_Img_Set";
+
+    //Color Names
+    public static final String COLOR_RED = "Red";
+    public static final String COLOR_ORANGE = "Orange";
+    public static final String COLOR_YELLOW = "Yellow";
+    public static final String COLOR_GREEN = "Green";
+    public static final String COLOR_CYAN = "Cyan";
+    public static final String COLOR_AZURE = "Azure";
+    public static final String COLOR_BLUE = "Blue";
+    public static final String COLOR_VIOLET = "Violet";
+    public static final String COLOR_MAGENTA = "Magenta";
+    public static final String COLOR_ROSE = "Rose";
+
+    //Default values
+    public static final String DEFAULT_TITLE = "New Location";
 }
