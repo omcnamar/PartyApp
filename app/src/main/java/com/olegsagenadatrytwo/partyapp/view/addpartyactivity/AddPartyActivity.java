0f17525fdc80ce569ba2502da48d24d5ca8c9f91
RockDrigo10 <rodrigo.chavez10@gmail.com>
2017-09-21 19:53:38 -0400

    adding party in Profile plus more stuff

--- app/src/main/java/com/olegsagenadatrytwo/partyapp/view/addpartyactivity/AddPartyActivity.java
+++ app/src/main/java/com/olegsagenadatrytwo/partyapp/view/addpartyactivity/AddPartyActivity.java
@@ -1,20 +1,20 @@
 package com.olegsagenadatrytwo.partyapp.view.addpartyactivity;
 
 import android.Manifest;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.view.View;
@@ -22,6 +22,7 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -70,6 +71,22 @@ public class AddPartyActivity extends AppCompatActivity implements AddPartyActiv
     TextInputLayout tvEndTime;
     @BindView(R.id.tvMinAge)
     TextInputLayout tvMinAge;
+    @BindView(R.id.action_backbutton)
+    ImageButton actionBackbutton;
+    @BindView(R.id.action_add_party)
+    ImageButton actionAddParty;
+    @BindView(R.id.action_profileSettings)
+    ImageButton actionProfileSettings;
+    @BindView(R.id.tvAddress)
+    TextInputLayout tvAddress;
+    @BindView(R.id.tvDescription)
+    TextInputLayout tvDescription;
+    @BindView(R.id.tvDate)
+    TextInputLayout tvDate;
+    @BindView(R.id.tvEndDate)
+    TextInputLayout tvEndDate;
+    @BindView(R.id.tvCapacity)
+    TextInputLayout tvCapacity;
 
     private AddPartyActivityPresenter presenter;
     private Party party;
@@ -89,6 +106,9 @@ public class AddPartyActivity extends AppCompatActivity implements AddPartyActiv
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_party);
         ButterKnife.bind(this);
+        //hiding party and settings buttons from toolbar
+        actionAddParty.setVisibility(View.INVISIBLE);
+        actionProfileSettings.setVisibility(View.INVISIBLE);
         //set up presenter
         presenter = new AddPartyActivityPresenter();
         presenter.attachView(this);
@@ -120,7 +140,7 @@ public class AddPartyActivity extends AppCompatActivity implements AddPartyActiv
                     if (ActivityCompat.checkSelfPermission(AddPartyActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                         ActivityCompat.requestPermissions(AddPartyActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PICK_FROM_GALLERY);
                     } else {
-                        Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                         startActivityForResult(galleryIntent, PICK_FROM_GALLERY);
                     }
                 } catch (Exception e) {
@@ -133,13 +153,12 @@ public class AddPartyActivity extends AppCompatActivity implements AddPartyActiv
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults)
-    {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
         switch (requestCode) {
             case PICK_FROM_GALLERY:
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                     startActivityForResult(galleryIntent, PICK_FROM_GALLERY);
                 } else {
                     //do something like displaying a message that he didn`t allow the app to access gallery and you wont be able to let him select from gallery
@@ -152,9 +171,9 @@ public class AddPartyActivity extends AppCompatActivity implements AddPartyActiv
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == PICK_FROM_GALLERY && resultCode == RESULT_OK) {
             Uri selectedImage = data.getData();
-            String[] filePathColumn = { MediaStore.Images.Media.DATA };
-            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
-            if(cursor != null) {
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            if (cursor != null) {
                 cursor.moveToFirst();
                 int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
                 String picturePath = cursor.getString(columnIndex);
@@ -209,8 +228,7 @@ public class AddPartyActivity extends AppCompatActivity implements AddPartyActiv
         if ((mEtCapacity.getText().toString().equals(""))) {
             mEtCapacity.setError("Capacity is required!");
             valid = false;
-        }
-        else if (Integer.parseInt(mEtCapacity.getText().toString()) >= 2147483647) {
+        } else if (Integer.parseInt(mEtCapacity.getText().toString()) >= 2147483647) {
             mEtCapacity.setError("Capacity is to big!");
             valid = false;
         }
@@ -289,12 +307,12 @@ public class AddPartyActivity extends AppCompatActivity implements AddPartyActiv
 
     private void showStartTime(String hour, String minute, String val) {
         //checking for time
-        if(Integer.parseInt(hour) < 12) {
+        if (Integer.parseInt(hour) < 12) {
             am_pm = "AM";
 
         } else {
             am_pm = "PM";
-            hour = String.valueOf(Integer.parseInt(hour) -12);
+            hour = String.valueOf(Integer.parseInt(hour) - 12);
         }
         if (minute.length() == 1) {
             minutes = new StringBuilder().append("0").append(minute);
@@ -309,7 +327,6 @@ public class AddPartyActivity extends AppCompatActivity implements AddPartyActiv
             mEtEndTime.setText(new StringBuilder().append(hour).append(":").append(minute).append(" ").append(am_pm));
 
     }
-
 
     @OnClick({R.id.etDate, R.id.etStartTime, R.id.etEndDate, R.id.etEndTime})
     public void onClick(View view) {
@@ -329,6 +346,15 @@ public class AddPartyActivity extends AppCompatActivity implements AddPartyActiv
             case R.id.etEndTime:
                 showDialog(DIALOG_TIME);
                 value = "end";
+                break;
+        }
+    }
+
+    @OnClick({R.id.action_backbutton})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.action_backbutton:
+                onBackPressed();
                 break;
         }
     }
