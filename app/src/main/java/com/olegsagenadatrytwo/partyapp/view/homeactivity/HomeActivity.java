94e1f423c92a5e813605fe886e22dff5a4f2216b
aaronhoskins2011 <aaronhoskins2011@gmail.com>
2017-09-22 02:27:17 -0400

    Fixed runtime permissions

--- app/src/main/java/com/olegsagenadatrytwo/partyapp/view/homeactivity/HomeActivity.java
+++ app/src/main/java/com/olegsagenadatrytwo/partyapp/view/homeactivity/HomeActivity.java
@@ -1,11 +1,14 @@
 package com.olegsagenadatrytwo.partyapp.view.homeactivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.content.ContextCompat;
@@ -50,6 +53,8 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import static com.olegsagenadatrytwo.partyapp.Constant.MY_PERMISSIONS_REQUEST_READ_LOCATION;
+
 public class HomeActivity extends AppCompatActivity implements HomeActivityContract.view {
 
     private static final String TAG = "HomeActivity";
@@ -75,6 +80,7 @@ public class HomeActivity extends AppCompatActivity implements HomeActivityContr
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        runtimePermission();
         ButterKnife.bind(this);
         DaggerHomeActivityComponent.create().inject(this);
         presenter.attachView(this);
@@ -176,7 +182,7 @@ public class HomeActivity extends AppCompatActivity implements HomeActivityContr
                         adapter.notifyDataSetChanged();
                     }
                 });
-               
+
                 adapter.notifyDataSetChanged();
             }
 
@@ -279,5 +285,63 @@ public class HomeActivity extends AppCompatActivity implements HomeActivityContr
     }
 
     public void goToLocation(View view) {
+    }
+
+    //                       //
+    //  Runtime Permission   //
+    //=======================//
+    public void runtimePermission() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_READ_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+    }
+
+    //                       //
+    //  Permission Result    //
+    //=======================//
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    
+
+                } else {
+
+
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
